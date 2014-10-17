e7983f2c5982c96f4a7fceeae81cd16e8dd36636
Talayhan <samet.talayhan@gmail.com>
2014-10-17 08:09:49 -0700

    added gameover script

---
+++ HW01/Scripts/GameOver.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour {
+
+	public GameObject object1;
+	public GameObject object2;
+
+
+	// Update is called once per frame
+	void Update () {
+		if (AttackCenter.isDone == true)
+		{
+			object1.renderer.enabled = true;
+			object2.renderer.enabled = true;
+		}
+	}
+}
